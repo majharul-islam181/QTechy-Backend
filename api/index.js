374a8c097158eb8977bcf4a2bd3116e372a70533
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-05-02 00:01:43 +0600

    Refactor file paths and update start scripts

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
-const app = require('./src/app'); // your express app
+const app = require('../src/app'); // your express app
 
 module.exports = serverless(app);
